d8c87213e4d1530d23b6b7f85ced9df8268363c6
Dachacho <arevadzedachi123@gmail.com>
2026-04-17 00:42:17 +0400

    add basic parsing and r

--- main.php
+++ main.php
@@ -29,7 +29,7 @@ while ($i < $length)
 {
     $char = $file[$i];
 
-    switch($char)
+    switch(true)
     {
         case(ctype_space($char)):
             $i++;
@@ -83,56 +83,91 @@ while ($i < $length)
             //skip closing quote
             $i++;
             break;
-        switch(true){
-            case ($char === '-'):
-            case (ctype_digit($char)):
-                $start = $i;
-
-                while($i < $length && ctype_digit($file[$i]))
-                {
-                    $i++;
-                }
-
-                $num_value = substr($file, $start, $i - $start); 
-                $tokens[] = ['TYPE' => TokenType::NUMBER, 'VALUE' => $num_value]; 
-                break;
-        }
+        case ($char === '-') || (ctype_digit($char)):
+            $start = $i;
+            
+            while($i < $length && ctype_digit($file[$i]))
+            {
+                $i++;
+            }
+            
+            $num_value = substr($file, $start, $i - $start); 
+            $tokens[] = ['TYPE' => TokenType::NUMBER, 'VALUE' => $num_value]; 
+            break;
     }
 }
 
-$accumulator = [];
 $i = 0;
 
-function parseObject(&$i)
+function parse_object(&$i, $tokens)
 {
-    $accumulator = [];
+    $result = [];
     $i++;
-    return parseMembers($i, $accumulator);
+    return parse_members($i, $tokens, $result);
 }
 
-function parseMembers(&$i, $accumulator)
+function parse_members(&$i, $tokens, $acc)
 {  
+    if($tokens[$i]['TYPE'] === TokenType::RIGHT_BRACKET)
+    {
+        $i++;
+        return $acc;
+    }
 
+    [$key, $value] = parse_pair($i, $tokens);
+    $acc[$key] = $value;
+
+    while($tokens[$i]['TYPE'] === TokenType::COMMA)
+    {
+        $i++;
+        [$key, $value] = parse_pair($i, $tokens);
+        $acc[$key] = $value; 
+    } 
+
+    if($tokens[$i]['TYPE'] !== TokenType::RIGHT_BRACKET)
+    {
+        throw new Exception("Expected '}' at the end");
+    }
+
+    $i++;
+    return $acc;
 }
 
-function parsePair()
+function parse_pair(&$i, $tokens)
 {
+    if ($tokens[$i]['TYPE'] !== TokenType::STRING) {
+        throw new Exception("Expected string key");
+    }
+    $key = $tokens[$i]['VALUE'];
 
+    $i++;
+    if($tokens[$i]['TYPE'] !== TokenType::COLON)
+    {
+        throw new Exception("Expected ':' after key");
+    }
+
+    $i++;
+    $value = parse_value($i, $tokens);
+    return [$key, $value];
 }
 
-function parseValue()
+function parse_value(&$i, $tokens)
 {
+    $token = $tokens[$i];
 
+    switch($token['TYPE'])
+    {
+        case TokenType::STRING:
+            $i++;
+            return $token['VALUE'];
+        case TokenType::NUMBER:
+            $i++;
+            return (int)$token['VALUE'];
+        case TokenType::LEFT_BRACKET:
+            return parse_object($i, $tokens);
+        default:
+            throw new Exception("Unexpected token");
+    }
 }
 
-// while($i < $length)
-// {
-//     switch($tokens[$i]['TYPE'])
-//     {
-//         case TokenType::LEFT_BRACKET:
-//             $i++;
-//             break;
-//         case TokenType::STRING:
-
-//     }
-// }
+var_dump(parse_object($i, $tokens));
